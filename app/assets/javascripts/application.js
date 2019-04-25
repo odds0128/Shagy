c545d7cf2d8a113dce467bc1cc9e943448a68d0c
Ryoya Funato <fff.100necdt@asagi.waseda.jp>
2019-04-25 20:26:29 +0900

    update: Tidy up vendor directory for appliyng bootstrap layout
    vendorフォルダを整理してbootstrapのレイアウトが当たるように変更．
    
    ※ タグ
    ・fix：バグ修正
    ・add：新規（ファイル）機能追加
    ・update : 機能修正．(バグではない)
    ・remove : 削除(ファイル)
    
    ・hotfix：クリティカルなバグ修正
    ・change：仕様変更
    ・clean：整理（リファクタリング等）
    ・disable：無効化（コメントアウト等）
    ・remove：削除（ファイル）
    ・upgrade：バージョンアップ
    ・revert：変更取り消し

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require_tree ../../../vendor/assets/js
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
